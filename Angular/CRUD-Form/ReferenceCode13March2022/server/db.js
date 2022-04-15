980747dcd3d960a8eb4507688a08dd8bdf4cd09e
nd-09 <100258788+nd-09@users.noreply.github.com>
2022-04-15 14:52:02 +0530

    Delete Angular directory

--- Angular/CRUD-Form/ReferenceCode13March2022/server/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose =require('mongoose');
-const url="mongodb://localhost:27017/CrudDB";
-
-mongoose.connect(url,(err,db)=>{
-    if(err)throw err;
-    console.log("Database Connection Succeeded..!!");
-});
-
-module.exports=mongoose;
